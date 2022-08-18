510134304558fc119a90d6856155d494131a85fc
Omarhuba <omaruba@gmail.com>
2022-08-18 21:54:30 +0200

    added useEffect and highlight animation in HeaderCartButton, and also use setTimeout and clearTimeout with better effect.

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,35 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 import styles from '../../Modules/HeaderCartButton.module.scss'
 import CartContext from '../../store/CartContext'
 import CartIcon from '../Cart/CartIcon'
 
 export const HeaderCartButton = (props) => {
+    const [btnHighLight, setBtnHighLight] = useState(false)
     const cartCtx = useContext(CartContext)
 
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
         return curNumber + item.amount
     }, 0)
 
-  return <button className={styles.button} onClick={props.onClick}>
+
+  const btnClasses = `${styles.button} ${ btnHighLight ? styles.bump : ''}`
+
+  useEffect(()=>{
+    if(cartCtx.items.length === 0){
+        return
+    }
+    setBtnHighLight(true);
+
+    const timer  = setTimeout(()=>{
+        setBtnHighLight(false)
+    },300)
+
+    return()=>{
+        clearTimeout(timer)
+    }
+  },[cartCtx])
+
+  return <button className={btnClasses} onClick={props.onClick}>
     <span className={styles.icon}>
         <CartIcon/>
     </span>
